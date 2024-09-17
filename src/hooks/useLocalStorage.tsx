12d41e574b2302fdf78bd455f87c5b9d5ee43a18
Shashank Trivedi <mdiffshashank@gmail.com>
2024-09-17 23:02:33 +0530

    more hooks added

---
+++ src/hooks/useLocalStorage.tsx
@@ -0,0 +1,28 @@
+import { useState } from "react";
+
+export function useLocalStorage(key: string, initialValue: string | Function) {
+  const [storedValue, setStoredValue] = useState(() => {
+    try {
+      const item = window.localStorage.getItem(key);
+      return item ? JSON.parse(item) : initialValue;
+    } catch (error) {
+      console.error(error);
+      return initialValue;
+    }
+  });
+
+  const setValue = (value: string | Function) => {
+    try {
+      const valueToStore =
+        value instanceof Function ? value(storedValue) : value;
+      setStoredValue(valueToStore);
+      window.localStorage.setItem(key, JSON.stringify(valueToStore));
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return [storedValue, setValue];
+}
+
+//use Case : Persisting state between page reloads, saving user preferences (e.g., dark mode), form drafts, etc.
